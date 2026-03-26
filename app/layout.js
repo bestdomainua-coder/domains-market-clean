99ac64b0928157cf83677015caa09139d508a943
bestdomainua-coder <bestdomainua@gmail.com>
2026-03-26 17:20:08 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-export default function RootLayout({ children }: any) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0 }}>{children}</body>
